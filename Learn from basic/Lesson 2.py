78dc0aa4ea35a0ac72e7808e97a9d7b258cf68af
liemqt77 <pietersonwill@gmail.com>
2025-08-05 11:56:50 +0700

    Deskripsi perubahan terbaru -menambahkan lesson 2

---
+++ Learn from basic/Lesson 2.py
@@ -0,0 +1,6 @@
+nama = "Liem"
+kelas = 11
+pelajaran = "Koding"
+
+print(nama+str(kelas)+pelajaran) #contoh concatenation
+print(f"Nama saya adalah {nama}, saya kelas {kelas} dan pelajaran yang saya suka adalah {pelajaran}") #f string
