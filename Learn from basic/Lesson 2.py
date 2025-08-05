d8877abb8578561d24c37e9ac2c871c771241611
liemqt77 <pietersonwill@gmail.com>
2025-08-05 11:59:20 +0700

    Deskripsi perubahan terbaru -menambahkan lesson 2

--- Learn from basic/Lesson 2.py
+++ Learn from basic/Lesson 2.py
@@ -1,6 +1,16 @@
 nama = "Liem"
 kelas = 11
 pelajaran = "Koding"
+x = 2
+y = "3"
 
 print(nama+str(kelas)+pelajaran) #contoh concatenation
 print(f"Nama saya adalah {nama}, saya kelas {kelas} dan pelajaran yang saya suka adalah {pelajaran}") #f string
+
+print(x+int(y)) #perubahan tipe data
+
+"""
+contoh perubahan tipe data seperti ini, misalkan ingin mengubah data dari string ke integer maka kita perlu
+menambahkan sintaks int(#masukan nama variabelnya) maka akan berubah, begitupun juga jika ingin mengubah
+dari int ke string
+"""
