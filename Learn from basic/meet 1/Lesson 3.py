315ee48f94b6931098f02d5f9b1876fdf2d9b414
liemqt77 <pietersonwill@gmail.com>
2025-08-06 10:26:36 +0700

    Struktur baru dan Lesson baru

---
+++ Learn from basic/meet 1/Lesson 3.py
@@ -0,0 +1,14 @@
+bil1 = 10 #variabel 1
+bil2 = 23 #variabel 2
+
+penambahan = bil1 + bil2 #perjumlahan
+print(penambahan)
+pengurangan = bil1 - bil2 #pengurangan
+print(pengurangan)
+perkalian = bil1 * bil2 #perkalian
+print(perkalian)
+
+print(f"{bil1} + {bil2} =  {penambahan}")
+print(f"{bil1} - {bil2} =  {pengurangan}")
+print(f"{bil1} * {bil2} =  {perkalian}")
+
---
+++ Learn from basic/meet 1/Lesson 4.py
@@ -0,0 +1,16 @@
+#assignment data
+contoh1 = 77
+contoh2 = 33
+
+print(contoh1)
+print(contoh2)
+
+contoh1 += contoh2
+print(contoh1) #assignment 1
+
+print(contoh1)
+
+contoh1 += contoh2
+print(contoh1) #assignment 2
+
+print(contoh1)
