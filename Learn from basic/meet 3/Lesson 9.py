3425a6e43d9c4467c2593197c91fe915d5a8e68b
liemqt77 <pietersonwill@gmail.com>
2025-08-09 10:32:18 +0700

    New Lesson

---
+++ Learn from basic/meet 3/Lesson 9.py
@@ -0,0 +1,26 @@
+#belajar tentang import
+
+import math
+
+x = math.sqrt(16)  # Menghitung akar kuadrat dari 16
+print(x)  # Output: 4.0
+
+import random
+
+x = random.randint(1, 100)  # Menghasilkan angka acak antara 1 dan 100
+print(x)
+
+from math import pi, sqrt #from nama_modul import nama_fungsi
+
+print(pi)  # Output: 3.141592653589793
+print(sqrt(25))  # Output: 5.0
+
+from random import choice
+
+list_item = ['apel', 'pisang', 'jeruk']
+print(choice(list_item))  # Menghasilkan salah satu item secara acak
+
+import modulsaya
+
+print(modulsaya.greet("Williem"))  # Output: Hello, Williem!
+
