cc711299d4840e84969aa279b9524ae746ec7389
liemqt77 <pietersonwill@gmail.com>
2025-08-09 10:34:45 +0700

    New Lesson

--- Learn from basic/meet 3/Lesson 10.py
+++ Learn from basic/meet 3/Lesson 10.py
@@ -5,3 +5,29 @@ def say_hello():
 
 # Memanggil fungsi
 say_hello()
+
+def greet(name):
+    print(f"Hello, {name}!")
+
+# Memanggil fungsi dengan memberikan argumen
+greet("Will")
+greet("Jo")
+
+def add(a, b):
+    return a + b
+
+result = add(5, 3)
+print(result)
+
+def multiply(x, y):
+    return x * y
+
+result = multiply(4, 5)
+print(result)
+
+def greet(name="Guest"):
+    print(f"Hello, {name}!")
+
+greet()  # Tidak memberikan argumen
+greet("Alice")  # Memberikan argumen
+
