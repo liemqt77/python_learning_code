a0b6bfd5a884ca0d58da802b2aa618fa6f8933f7
liemqt77 <pietersonwill@gmail.com>
2025-08-09 10:41:26 +0700

    New Lesson

---
+++ Learn from basic/meet 3/Lesson 11.py
@@ -0,0 +1,22 @@
+#belajar break, looping (for, while)
+
+while True: #kondisi terpenuhi
+    user_input = input("Type 'exit' to quit: ")
+    if user_input.lower() == 'loop':  # Menghentikan loop jika pengguna mengetik 'exit'
+        print("loop.")
+    elif user_input.lower()== 'exit':
+        print("You typed:", user_input)
+        break
+    else:
+        print("Out from loop")
+
+for counter in range(0,10):
+    print(f"This is for loop number {counter+1}")
+
+for i in range(0, 10):
+    print(i)
+
+fruits = ["apple", "banana", "cherry"]
+
+for fruit in fruits:
+    print(fruit)
